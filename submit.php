ee783f3732a45baaae6634a5b4f19018c3ca3525
Victoria N. Hargadon <vharga0074@my.sctd.edu>
2017-09-07 15:32:48 -0400

    Update submit.php

--- submit.php
+++ submit.php
@@ -32,6 +32,9 @@ if (isset($_SESSION["user_session"])) {
                 $file_loc = $_FILES['file']['tmp_name'];
                 $file_size = $_FILES['file']['size'];
                 $file_type = $_FILES['file']['type'];
+                $fileT = pathinfo($file);
+                $fileT['extension'];
+                $acceptedEx = Array('jpg', 'png', 'jpeg', 'txt');
                 $folder = "uploads/";
                 $new_size = $file_size / 1024;
                 $username = $_SESSION["user_session"];
@@ -40,15 +43,23 @@ if (isset($_SESSION["user_session"])) {
                 $username = mysqli_real_escape_string($dbc, $username);
                 $new_file_name = strtolower($file);
                 $final_file = str_replace(' ', '-', $new_file_name);
-                if (move_uploaded_file($file_loc, $folder . $final_file)) {
-                    if ($file_type == 'image/jpeg' || $file_type == 'image/gif' || $file_type == 'image/png' || $file_type == 'image/jpeg'|| $file_type == 'text/plain') {
-                        //is image
-                        $q = mysqli_query($dbc, "INSERT INTO file (Title,username,type,size,file) VALUES ('$titleV','$username','$file_type','$new_size','$final_file')") OR die(mysqli_error_list($dbc));
-                        ob_start();
-                        header("Location: index.php");
+                if (in_array($fileT['extension'], $acceptedEx)) {
+                    if ($new_size > 500000) {
+                        if (move_uploaded_file($file_loc, $folder . $final_file)) {
+                            if ($file_type == 'image/jpeg' || $file_type == 'image/gif' || $file_type == 'image/png' || $file_type == 'image/jpeg' || $file_type == 'text/plain') {
+                                //is image
+                                $q = mysqli_query($dbc, "INSERT INTO file (Title,username,type,size,file) VALUES ('$titleV','$username','$file_type','$new_size','$final_file')") OR die(mysqli_error_list($dbc));
+                                ob_start();
+                                header("Location: index.php");
+                            } else {
+                                echo '<p id="signupMess">File type not accepted!</p>';
+                            }
+                        }
                     } else {
-                        echo '<p id="signupMess">File type not accepted!</p>';
+                        echo '<p id="signupMess">File size too large</p>';
                     }
+                } else {
+                    echo '<p id="signupMess">File type not accepted!</p>';
                 }
             }
         }
@@ -66,10 +77,10 @@ if (isset($_SESSION["user_session"])) {
         </head>
         <body>
             <nav id="navbar"><img src="" id="logo"><a href="index.php" class="button">Browse</a>  <a href="submit.php" class="button">Submit</a>  <a href="profile.php" class="button">Profile</a><?php
-    if (!isset($_SESSION["user_session"])) {
-        echo '<a href="login.php" id="loginButton">Login</a><a href="signUp.php" id="signUpbutton">Sign Up</a>';
-    }
-    ?> </nav>
+                if (!isset($_SESSION["user_session"])) {
+                    echo '<a href="login.php" id="loginButton">Login</a><a href="signUp.php" id="signUpbutton">Sign Up</a>';
+                }
+                ?> </nav>
             <p class="loginErr">Please login to submit your files!</p>
             <?php
         }
